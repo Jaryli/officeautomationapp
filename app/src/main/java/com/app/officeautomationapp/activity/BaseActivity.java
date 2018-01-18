1002be7ffe7ffa634bb13069ab5bee7ac6d0d93f
yuqin <361636565@qq.com>
2018-01-18 22:39:22 +0800

    百度地圖高精度

--- app/src/main/java/com/app/officeautomationapp/activity/BaseActivity.java
+++ app/src/main/java/com/app/officeautomationapp/activity/BaseActivity.java
@@ -1,9 +1,15 @@
 package com.app.officeautomationapp.activity;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.app.officeautomationapp.R;
 
@@ -18,6 +24,22 @@ public class BaseActivity extends FragmentActivity {
         Log.d("BaseActivity",getClass().getSimpleName());
         setContentView(R.layout.activity_base);
         ActivityCollector.addActivity(this);
+        //Android 6.0判断用户是否授予定位权限
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {//如果 API level 是大于等于 23(Android 6.0) 时
+//            //判断是否具有权限
+//            if (ContextCompat.checkSelfPermission(this,
+//                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//                //判断是否需要向用户解释为什么需要申请该权限
+//                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+//                        Manifest.permission.ACCESS_COARSE_LOCATION)) {
+//                    Toast.makeText(BaseActivity.this,"自Android 6.0开始需要打开位置权限",Toast.LENGTH_SHORT).show();
+//                }
+//                //请求权限
+//                ActivityCompat.requestPermissions(this,
+//                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+//                        REQUEST_CODE_ACCESS_COARSE_LOCATION);
+//            }
+//        }
     }
 
     @Override
--- app/src/main/java/com/app/officeautomationapp/activity/MapActivity.java
+++ app/src/main/java/com/app/officeautomationapp/activity/MapActivity.java
@@ -4,10 +4,14 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.app.officeautomationapp.R;
+import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.BitmapDescriptor;
 import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.MyLocationConfiguration;
 import com.baidu.mapapi.map.OverlayOptions;
 import com.baidu.mapapi.map.TextureMapView;
 import com.baidu.mapapi.model.LatLng;
@@ -25,14 +29,18 @@ public class MapActivity extends BaseActivity implements View.OnClickListener  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_map);
         double lon=getIntent().getDoubleExtra("lon",39.963175);
         double lati=getIntent().getDoubleExtra("lati",116.400244);
         mMapView = (TextureMapView) findViewById(R.id.mTexturemap);
         mBaiduMap = mMapView.getMap();
 
         mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);//普通地图 ,mBaiduMap是地图控制器对象
-
-        LatLng point = new LatLng(lon, lati);
+        mBaiduMap.setMyLocationConfiguration(new MyLocationConfiguration(
+                MyLocationConfiguration.LocationMode.FOLLOWING, true, BitmapDescriptorFactory .fromResource(R.mipmap.icon_marka),
+                0xAAFFFF88, 0xAA00FF00));
+        LatLng point = new LatLng(lati, lon);
         //构建Marker图标
 
         BitmapDescriptor bitmap = BitmapDescriptorFactory
@@ -48,6 +56,8 @@ public class MapActivity extends BaseActivity implements View.OnClickListener  {
 
         mBaiduMap.addOverlay(option);
 
+        MapStatusUpdate status = MapStatusUpdateFactory.newLatLng(point);
+        mBaiduMap.animateMapStatus(status);
     }
 
     @Override
